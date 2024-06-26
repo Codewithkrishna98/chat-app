7c744272c5bdf7a4b79cf20126c1bbba758bd97a
Codewithkrishna98 <143336373+Codewithkrishna98@users.noreply.github.com>
2024-06-26 14:03:50 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const server = createServer(app)
 
 const io = new Server(server , {
   cors:{
-    origin:"http://localhost:5173",
+    origin:"https://kishangupta-made2automate.netlify.app/",
     methods:["GET","POST"],
     credentials:true
   }
